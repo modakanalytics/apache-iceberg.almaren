5a24a5f47bebb101b815d647904d553f161eed4a
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2022-03-01 13:41:23 -0300

    add write support with back compability

--- src/main/scala/com/github/music/of/the/ainur/almaren/iceberg/IcebergConnector.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/iceberg/IcebergConnector.scala
@@ -5,6 +5,10 @@ import org.apache.spark.sql.SaveMode.{Append,Overwrite,ErrorIfExists,Ignore}
 import com.github.music.of.the.ainur.almaren.Tree
 import com.github.music.of.the.ainur.almaren.builder.Core
 import com.github.music.of.the.ainur.almaren.state.core.{Target,Source}
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.CreateTableWriter
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.DataFrameWriterV2
 
 private[almaren] case class SourceIceberg(table:String, options:Map[String,String]) extends Source {
   def source(df: DataFrame): DataFrame = {
@@ -15,21 +19,42 @@ private[almaren] case class SourceIceberg(table:String, options:Map[String,Strin
   }
 }
 
-private[almaren] case class TargetIceberg(table:String, options:Map[String,String],saveMode:SaveMode) extends Target {
+private[almaren] case class TargetIceberg(table:String, options:Map[String,String],saveMode:SaveMode,partitionedBy:Column*) extends Target {
   def target(df: DataFrame): DataFrame = {
-    logger.info(s"table:{$table}, options:{$options}, saveMode:{$saveMode}")
+    logger.info(s"table:{$table}, options:{$options}, saveMode:{$saveMode}, partitionedBy:{$partitionedBy}")
     val write = df
     .writeTo(table)
     .options(options)
 
-    saveMode match {
-      case Append => write.append()
-      case Overwrite => write.overwritePartitions()
-      case ErrorIfExists => throw new Exception("""Mode "ErrorIfExists"" not supported""")
-      case Ignore => write.createOrReplace()
+    if(partitionedBy.isEmpty) {
+      saveMode match {
+        case Append => write.append()
+        case Overwrite => write.createOrReplace()
+        case ErrorIfExists => checkIfTableExists(df,table,Right(write))
+        case Ignore => write.createOrReplace()
+      }
+    } 
+    else {
+      val withPartition = write.partitionedBy(partitionedBy.head,partitionedBy.tail:_*)
+      saveMode match {
+        case Append => write.append()
+        case Overwrite => withPartition.createOrReplace()
+        case ErrorIfExists => checkIfTableExists(df,table,Left(withPartition))
+        case Ignore => withPartition.createOrReplace()
+      }
     }
-        df
+    df
   }
+
+  private def checkIfTableExists[T](df:DataFrame,tableName:String,writer:Either[CreateTableWriter[T],DataFrameWriterV2[T]]): Unit = 
+     if(df.sparkSession.catalog.tableExists(tableName = table))
+          throw new Exception(s"Table $table already exists")
+     else 
+        writer match {
+          case Left(l) => l.create()
+          case Right(r) => r.create()
+        }
+
 }
 
 private[almaren] trait IcebergConnector extends Core {
